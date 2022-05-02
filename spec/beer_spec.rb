fbfe443778b87ac069a13acec1e335f51c711e00
Sören Klatt <s.klatt@mw-intern.de>
2022-05-02 09:37:24 +0200

    created the first beer

--- spec/beer_spec.rb
+++ spec/beer_spec.rb
@@ -3,7 +3,25 @@ RSpec.describe Beer do
     expect(Beer::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'Noice Pils' do
+    it "has the right brewery name" do
+      expect(Beer::Noice.brewery).to eq('Welde')
+    end
+
+    it "has the right origin" do
+      expect(Beer::Noice.origin).to eq('Plankstadt')
+    end
+
+    it "has the right alcohol_concentration" do
+      expect(Beer::Noice.alcohol_concentration).to eq('4,8 %')
+    end
+
+    it "has the right bitterness" do
+      expect(Beer::Noice.bitterness).to eq('28 IBU')
+    end
+
+    it "has the right original_wort" do
+      expect(Beer::Noice.original_wort).to eq('11°P')
+    end
   end
 end
