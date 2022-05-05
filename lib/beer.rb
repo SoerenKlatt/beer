9dbc3ec41c140276fc5daadfb43265e837376ba2
Sören Klatt <s.klatt@mw-intern.de>
2022-05-05 13:19:57 +0200

    created noice

--- lib/beer.rb
+++ lib/beer.rb
@@ -2,5 +2,26 @@ require "beer/version"
 
 module Beer
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Noice
+    def self.brewery
+      'Welde'
+    end
+
+    def self.origin
+      'Plankstadt'
+    end
+
+    def self.alcohol_concentration
+      '4,8 %'
+    end
+
+    def self.bitterness
+      '28 IBU'
+    end
+
+    def self.original_wort
+      '11°P'
+    end
+  end
 end
