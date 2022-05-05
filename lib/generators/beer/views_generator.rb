d745eafc6033a99649e8b4a6e6fd6d1a824de371
Sören Klatt <s.klatt@mw-intern.de>
2022-05-05 09:03:38 +0200

    changed function name

--- lib/generators/beer/views_generator.rb
+++ lib/generators/beer/views_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators/base'
 module Beer
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      def create_initializer_file
+      def create_index_view_file
         create_file "app/views/beer/index.html.erb", "# Add index view"
       end
     end
