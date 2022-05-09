de44b6d5dc262f4da60b124440259c1046566f79
Sören Klatt <s.klatt@mw-intern.de>
2022-05-09 10:02:42 +0200

    obsolete

--- lib/generators/beer/install_generator.rb
+++ lib/generators/beer/install_generator.rb
@@ -1,20 +1,7 @@
 module SimpleForm
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      argument :group, type: :string
-      argument :beer, type: :string
-
-      def create_group
-        empty_directory(group)
-      end
-
-      def copy_overview
-        template("beer_overview.txt", "#{group}/#{beer}.txt")
-      end
-
-      def self.source_root
-        File.dirname(__FILE__) + "/templates"
-      end
+      source_root File.expand_path('templates', __dir__)
     end
   end
 end
